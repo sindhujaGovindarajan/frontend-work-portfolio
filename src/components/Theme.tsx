f58edfe31bc1fc2ea04e410cce186786530d61cd
sindhuja Govindarajan <srin2258@gmail.com>
2025-03-11 17:43:00 +0000

    wip: Theme HOC

---
+++ src/components/Theme.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import Switch from "./Switch"; // Import the generic Switch component
+
+interface ThemeProps {
+  children: React.ReactNode;
+}
+
+const ThemeContainer = styled.div`
+  // display: flex;
+  display: none;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+const ThemeSpan = styled.span`
+  font-size: 0.9rem;
+  margin-left: 10px;
+`;
+
+// Define themes
+const lightTheme = {
+  background: "#fff",
+  color: "#333",
+  cardBackground: "#fff",
+  cardColor: "#333",
+  chipBackground: "#d4efdf",
+  chipHover: "#aed6b8",
+};
+
+const darkTheme = {
+  background: "#333",
+  color: "#fff",
+  cardBackground: "#444",
+  cardColor: "#eee",
+  chipBackground: "#555",
+  chipHover: "#666",
+};
+
+// Global style for theme
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.background};
+    color: ${(props) => props.theme.color};
+    transition: background-color 0.3s ease, color 0.3s ease;
+  }
+`;
+
+const Theme: React.FC<ThemeProps> = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      {children}
+      <ThemeContainer>
+        <Switch checked={theme === "dark"} onChange={toggleTheme} />
+        <ThemeSpan>{theme === "light" ? "Light" : "Dark"}</ThemeSpan>
+      </ThemeContainer>
+    </ThemeProvider>
+  );
+};
+
+export default Theme;
