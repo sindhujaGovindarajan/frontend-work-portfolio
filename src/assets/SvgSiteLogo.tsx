e348d20e42bf8da298a20cb19f92448a38750198
sindhuja Govindarajan <srin2258@gmail.com>
2025-03-13 01:27:26 +0000

    SiteLogo update

--- src/assets/SvgSiteLogo.tsx
+++ src/assets/SvgSiteLogo.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import styled from "styled-components";
 
 interface SvgSiteLogoProps extends React.SVGAttributes<SVGElement> {
   style?: React.CSSProperties;
 }
 
-const SvgSiteLogo = (props: SvgSiteLogoProps) => (
+const SiteLogo = (props: SvgSiteLogoProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300" {...props}>
     <circle cx={150} cy={150} r={135} fill="#5831D9" />
     <defs>
@@ -19,15 +20,40 @@ const SvgSiteLogo = (props: SvgSiteLogoProps) => (
       strokeLinecap="round"
       strokeWidth={24}
       d="M150 75c-50 0-60 60 0 60s50 60 0 60"
-      filter="url(#siteLogo_svg__boxShadow)"
     />
-    <circle
-      cx={150}
-      cy={225}
-      r={10}
-      fill="#31D93C"
-      filter="url(#siteLogo_svg__boxShadow)"
-    />
+    <circle cx={150} cy={225} r={10} fill="#31D93C" />
   </svg>
 );
+
+const SvgSiteLogo = styled(SiteLogo)`
+  height: clamp(40px, 6vw, 60px);
+  width: auto;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  display: block;
+
+  @media (max-width: 1200px) {
+    height: 50px;
+    margin-right: 7px;
+    margin-bottom: 7px;
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    margin-right: 6px;
+    margin-bottom: 6px;
+  }
+
+  @media (max-width: 576px) {
+    height: 40px;
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
+
+  @media (max-width: 320px) {
+    height: 35px;
+    margin-right: 4px;
+    margin-bottom: 4px;
+  }
+`;
 export default SvgSiteLogo;
